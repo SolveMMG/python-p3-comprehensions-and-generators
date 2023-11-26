f572d1433d4ed2f41eff6b63330cf2247756cc40
Your Name <mikachupichu@gmail.com>
2023-11-27 01:20:39 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [num for num in num_list if num % 2 == 0]
+    return even_numbers
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    add_exclamation = [sen + "!" for sen in sentence_list ]
+    return add_exclamation
+
